384ab5ff02819d71fe9189ebf9656dc4e1be4b01
lijiacen <lijiacen880915@163.com>
2020-11-02 16:50:40 +0800

    页面整体布局实现

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import App from "next/app";
-
 import "antd/dist/antd.css";
+import Layout from "../components/Layout";
 
-export default App;
+class MyApp extends App {
+  static async getInitialProps(ctx) {
+    const { Component } = ctx;
+    let pageProps;
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return {
+      pageProps
+    };
+  }
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
+    return (
+      //   <Provider store={reduxStore}>
+      //     {this.state.loading ? <PageLoading /> : null}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      //   </Provider>
+    );
+  }
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,19 @@
-// import React from "react";
-// function index() {
-//   return <div>index by function</div>;
-// }
-// export default index;
+import React from "react";
 
-// import React from "react";
-// class Index extends React.Component {
-//   render() {
-//     return <div>index by class</div>;
-//   }
-// }
-// export default Index;
-
-import React, { useState, useEffect } from "react";
-import "./test.css";
-import axois from "axios";
-import { Button } from "antd";
-
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-
-export default function indexHooks() {
-  const [index] = useState("index");
-
-  useEffect(() => {
-    axois.get("/users").then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-  return (
-    <div>
-      {index}
-      <a href={publicRuntimeConfig.github_oauth}>登录</a>
-      <Button>click it</Button>
-    </div>
-  );
+function Index() {
+  return <span>index</span>;
 }
+// Index.getInitialProps = async ({ ctx }) => {
+//   // const result = await axios
+//   //   .get("/github/search/repositories?q=react")
+//   //   .then((res) => {
+//   //     console.log(res);
+//   //   });
+//   const result = await api.request(
+//     { url: "/search/repositories?q=react" },
+//     ctx.req,
+//     ctx.res
+//   );
+//   return { data: result.data };
+// };
+export default Index;
